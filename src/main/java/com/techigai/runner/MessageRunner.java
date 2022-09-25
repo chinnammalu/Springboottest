8a65fad8d35419f61ae7878890187c1b6baf059a
chinnammalu.peddinti <chinnammalu.peddinti@TGU1DELL0187.techigai.local>
2022-09-25 20:03:46 +0530

    it is modified line by line

--- src/main/java/com/techigai/runner/MessageRunner.java
+++ src/main/java/com/techigai/runner/MessageRunner.java
@@ -8,8 +8,12 @@ public class MessageRunner  implements CommandLineRunner{
 	@Override
 	public void run(String... args) throws Exception {
 		// TODO Auto-generated method stub
+		System.out.println("manager runner ............");
 		System.out.println("Wlcome to Spring boot application");
+		System.out.println("manager runner ....####....@@@....");
 		System.out.println("File Modified...");
+		System.out.println("manager runner ....####........");
+
 
 	}
 
