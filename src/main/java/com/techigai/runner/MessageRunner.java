4773b684b7ea6579c70fc7b3b6d0eb28b2dc5f40
chinnammalu.peddinti <chinnammalu.peddinti@TGU1DELL0187.techigai.local>
2022-09-25 14:40:16 +0530

    WH-8520-First-Time-Commit

---
+++ src/main/java/com/techigai/runner/MessageRunner.java
@@ -0,0 +1,14 @@
+package com.techigai.runner;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+@Component
+public class MessageRunner  implements CommandLineRunner{
+
+	@Override
+	public void run(String... args) throws Exception {
+		// TODO Auto-generated method stub
+		System.out.println("Wlcome to Spring boot application");
+	}
+
+}
